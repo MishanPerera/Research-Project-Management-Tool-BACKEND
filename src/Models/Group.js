ebf39aa48b47d70ebb6dc6365361909e70590ff4
Mishan Perera <mishkavinduperera65@gmail.com>
2022-06-04 18:31:05 +0530

    update Group.js

--- src/Models/Group.js
+++ src/Models/Group.js
@@ -1,31 +1,40 @@
 const mongoose =require('mongoose');
 
+//supervisor & panel schema
 const GroupSchema = new mongoose.Schema({
+    
     leaderId:{
         type:String,
         required:true
     },
+
     supervisorId:{
         type:String,
     },
+
     name:{
         type:String,
         required:true
     },
+
     description:{
         type:String,
         required:true
     },
+
     members:{
         type:Array,
         required:true
     },
+
     isApprove:{
         type: String,
     },
+
     panelId:{
         type: String,
     },
+
     register_date:{
         type: Date,
         default: Date.now
