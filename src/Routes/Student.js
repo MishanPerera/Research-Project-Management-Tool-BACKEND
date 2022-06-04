8f0c7b76b4d7bb970892aa26431bc3486a464e17
Manuka_Dahanayake <manukaud98@gmail.com>
2022-06-04 18:52:22 +0530

    commit student route modification

--- src/Routes/Student.js
+++
@@ -1,8 +0,0 @@
-const express =require('express');
-const router= express.Router();
-const auth = require('../MiddleWare/auth');
-const Group = require('../Models/Group');
-const User = require('../Models/User');
-const Topic = require('../Models/Topic');
-
-router.post('')
