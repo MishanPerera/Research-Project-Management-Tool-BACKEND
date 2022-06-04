7a6c84b3caecd6b83ec033debd62e88bb5fb6cbb
Ninura.Jayasekara <ninura.jayasekara@gmail.com>
2022-06-04 08:39:13 +0530

    commit update password to supervisor route

--- src/Route/superviosrRoute.js
+++ src/Route/superviosrRoute.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 const router = express.Router()
 const upload = require('../utils/multer')
 
-const {registerSupervisor, supervisorLogin} = require ('../controller/supervisorController')
+const {registerSupervisor, supervisorLogin, updatePassword, forgotPassword} = require ('../controller/supervisorController')
 
 
 
@@ -11,5 +11,13 @@ router.post('/supervisorRegister' , registerSupervisor)
 
 
 router.post('/login', supervisorLogin)
+
+
+router.post('/forgotPassword', forgotPassword)
+
+//router.post('/postOTP', postOTP)
+
+//UPDATE PASSWORD
+router.post('/updatePassword', passport.authenticate('jwt', { session: false }), updatePassword)    
 
 module.exports = router
