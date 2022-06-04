5b49e3d052434462ddbccb5c93876c013616a23a
Ninura.Jayasekara <ninura.jayasekara@gmail.com>
2022-06-04 07:53:18 +0530

    commit supervisor route

---
+++ src/Route/superviosrRoute.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+const upload = require('../utils/multer')
+
+const {registerSupervisor, supervisorLogin} = require ('../controller/supervisorController')
+
+router.post('/supervisorRegister' , registerSupervisor)
+
+
+router.post('/login', supervisorLogin)
+
+module.exports = router
